abc1481a34693c3e29f61641dc8106e494076292
ctrl809 <84770547+ctrl809@users.noreply.github.com>
2024-02-26 20:01:36 +0700

    Create phplawas.sh

---
+++ phplawas.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# To add this repository please do:
+
+if [ "$(whoami)" != "root" ]; then
+    SUDO=sudo
+fi
+
+apt-get update
+apt-get -y install lsb-release ca-certificates curl
+curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+dpkg -i /tmp/debsuryorg-archive-keyring.deb
+sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+apt-get update
