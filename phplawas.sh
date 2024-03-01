c1f273ac60257270d6c086416eaac38869f7862e
ctrl809 <84770547+ctrl809@users.noreply.github.com>
2024-03-01 17:35:34 +0700

    Update phplawas.sh

--- phplawas.sh
+++ phplawas.sh
@@ -11,3 +11,4 @@ curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsury
 dpkg -i /tmp/debsuryorg-archive-keyring.deb
 sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 apt-get update
+echo "sekarang anda bisa menginstall php7 version"
